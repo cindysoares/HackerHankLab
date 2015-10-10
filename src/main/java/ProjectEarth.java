67340d1a6659b619bfa6729cdd673ae2471d421a
Cindy Soares <cindy.as@gmail.com>
2015-10-10 17:38:39 -0300

    better performance

--- src/main/java/ProjectEarth.java
+++ src/main/java/ProjectEarth.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class ProjectEarth {
 	
+	private static final BigInteger TWO = new BigInteger("2");
+
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		BigInteger n = s.nextBigInteger(), t = s.nextBigInteger();
@@ -12,10 +14,14 @@ public class ProjectEarth {
 	
 	static BigInteger reactionTime(BigInteger n, BigInteger t) {
 		BigInteger seconds = BigInteger.ONE;
-		for (BigInteger i = BigInteger.ONE; i.compareTo(n) <= 0; i=i.add(BigInteger.ONE)) {
-			if (seconds.mod(new BigInteger("2"))==BigInteger.ZERO) t=t.add(seconds);
+		for (BigInteger i = BigInteger.ONE; i.compareTo(n) < 0; i=i.add(TWO)) {
+			if (seconds.mod(TWO)==BigInteger.ZERO) t=t.add(seconds.multiply(TWO));
+			else t=t.subtract(seconds.multiply(TWO));
+			seconds=seconds.add(BigInteger.ONE); 
+		}
+		if (n.mod(TWO)!=BigInteger.ZERO) {
+			if (seconds.mod(TWO)==BigInteger.ZERO) t=t.add(seconds);
 			else t=t.subtract(seconds);
-			if(i.mod(new BigInteger("2"))==BigInteger.ZERO) seconds=seconds.add(BigInteger.ONE); 
 		}
 		return t;
 	}
--- src/test/java/ProjectEarthTest.java
+++ src/test/java/ProjectEarthTest.java
@@ -9,6 +9,9 @@ public class ProjectEarthTest {
 	@Test
 	public void test() {
 		Assert.assertEquals(new BigInteger("2"), ProjectEarth.reactionTime(new BigInteger("5"), new BigInteger("3")));
+		Assert.assertEquals(new BigInteger("4"), ProjectEarth.reactionTime(new BigInteger("4"), new BigInteger("2")));
+		Assert.assertEquals(new BigInteger("14"), ProjectEarth.reactionTime(new BigInteger("10"), new BigInteger("20")));
+		Assert.assertEquals(new BigInteger("5"), ProjectEarth.reactionTime(new BigInteger("11"), new BigInteger("5")));
 		Assert.assertEquals(new BigInteger("29999999999999999999999999999999999"), 
 				ProjectEarth.reactionTime(new BigInteger("5"), new BigInteger("30000000000000000000000000000000000")));
 	}
